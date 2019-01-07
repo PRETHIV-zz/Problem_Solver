18e3c2e10d1da470d05bfc8df0381001cdfa4fae
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-07 15:59:47 +0530

    Update greatestamongthree.py

--- greatestamongthree.py
+++ greatestamongthree.py
@@ -4,7 +4,8 @@ k=0
 if a>=b and a>=c:
   print(a)
   k=1
-elif b>=a and b>=c and k=0:
+elif b>=a and b>=c and k==0:
   print(b)
-elif c>=a and c>=b and k=0:
+  k=1
+elif c>=a and c>=b and k==0:
   print(c)
