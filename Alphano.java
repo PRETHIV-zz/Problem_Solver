67162f3bd2aaf8bb0b281ef3201be42839341127
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-07 15:15:08 +0530

    Create Alphano.java

---
+++ Alphano.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Alphano{
+
+public static void main(String args[]){
+  char cmp[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+  Scanner s=new Scanner(System.in);
+  char c;
+  c=s.next().charAt(0);
+  int k=0;
+  for(int i=0;i<cmp.length;i++){
+      if(c==cmp[i]){
+        k=1;
+        System.out.println("Alphabet");
+        break;
+      }
+  }
+  if(k=0){
+    System.out.println("No");
+  }
+}
+}
