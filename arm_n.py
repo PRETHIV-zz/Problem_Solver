13226a191e7ee5a88f9397f862fddde69d737737
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-08 09:26:51 +0000

    Create arm_n.py

---
+++ arm_n.py
@@ -0,0 +1,16 @@
+#it is my own code guvi
+n=int(input())
+on=n
+dig=0
+l=[]
+while on>0:
+    dig=dig+1
+    l.append(on%10)
+    on=on//10
+res=0
+for i in l:
+    res=res+(i**dig)
+if res==n:
+    print("Armstrong")
+else:
+    print("Not Armstrong")
