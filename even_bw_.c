23e48b2f3fee61da8d6665d601394a19788470c4
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-08 12:09:19 +0530

    Create even_bw_.c

---
+++ even_bw_.c
@@ -0,0 +1,14 @@
+//it is my own code guvi
+#include<stdio.h>
+int main(){
+	int i,k;
+	scanf("%d",&i);
+	scanf("%d",&k);
+	int j,l=k-1;
+	for(j=i+1;j<=l;j++){
+		if(j%2==0){
+			printf("%d ",j);
+		}
+	}
+	return 0;
+}
