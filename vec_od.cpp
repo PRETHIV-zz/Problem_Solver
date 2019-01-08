33e63216ecf88a39311ae677e9e44861b197090b
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-08 09:17:14 +0000

    Create vec_od.cpp

---
+++ vec_od.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int> v;
+	int l1,l2;
+	cin>>l1>>l2;
+	l1++;
+	for(int i=l1;i<l2;i++){
+		if(i%2==1){
+			v.push_back(i);
+		}
+	}
+	vector<int>::iterator b,e;
+	e=v.end();
+	for(b=v.begin();b!=e;b++){
+		if(b==e-1){
+			cout<<*b;
+		}
+		else{
+			cout<<*b<<" ";
+		}
+	}
+	return 0;
+}
+
