f9f742604f9caa480ec4e09d7516cfa9446869f6
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-09 13:45:24 +0530

    Create 13_no_of_splchar.py

---
+++ 13_no_of_splchar.py
@@ -0,0 +1,13 @@
+# Hello World program in Python
+s=input()
+c=0
+for i in s:
+    if i.isalpha():
+        continue
+    elif i.isnumeric():
+        continue
+    elif i.isspace():
+        continue
+    else:
+        c+=1
+print(c)
