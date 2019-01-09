10a93649b1bf6fe962d74867fa821fcbb0e58cb4
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-09 10:12:53 +0530

    Create 4_romn_to_num.py

---
+++ 4_romn_to_num.py
@@ -0,0 +1,8 @@
+#guvi this is rocky
+rom=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+stri=input()
+for i in range(len(rom)):
+    if rom[i]==stri:
+        print(num[i])
+        break
