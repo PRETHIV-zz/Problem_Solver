d9b2085106d39209d5523e9fd7aabb6220617c71
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-10 16:20:34 +0530

    Create 25_max_min.java

---
+++ 25_max_min.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=scan.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.print(Integer.toString(a[0])+" "+Integer.toString(a[n-1]));
+	}
+}
