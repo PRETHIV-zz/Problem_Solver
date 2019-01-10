ff2110dba16f63584ffa4f671189de7e115a4c27
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-10 16:15:44 +0530

    Create 24_next_no.java

---
+++ 24_next_no.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i;
+		Scanner ss=new Scanner(System.in);
+		i=ss.nextInt();
+		System.out.print(++i);
+	}
+}
