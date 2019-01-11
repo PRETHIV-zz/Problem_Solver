276ccb3d90508f13e51e6f1c75b80cf96e684504
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-11 16:58:24 +0530

    Create 52_N_in_range.py

---
+++ 52_N_in_range.py
@@ -0,0 +1,9 @@
+# Hello World program in Python
+#guvi this is rocky
+#rockys aim is getting output without using a single variable
+a=int(input())
+l,r=map(int,input().split(" "))
+if a in range(l+1,r):
+    print("yes")
+else:
+    print("no")
