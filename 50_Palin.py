12d07476cc59f6698deccb610e9c863179cc0b31
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-11 16:50:56 +0530

    Create 50_Palin.py

---
+++ 50_Palin.py
@@ -0,0 +1,11 @@
+# Hello World program in Python
+#guvi this is rocky
+#rockys aim is getting output without using a single variable
+a=input()
+b=""
+for i in a:
+    b=i+b
+if a==b:
+    print("yes")
+else:
+    print("no")
