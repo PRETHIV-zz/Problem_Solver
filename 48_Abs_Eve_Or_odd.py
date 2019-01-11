39b90c31d0c1a1b345ab60746f19f604129b450c
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-11 16:42:57 +0530

    Create 48_Abs_Eve_Or_odd.py

---
+++ 48_Abs_Eve_Or_odd.py
@@ -0,0 +1,8 @@
+# Hello World program in Python
+#guvi this is rocky
+#rockys aim is getting output without using a single variable
+a,b=map(int,input().split(" "))
+if abs(a-b)%2==0:
+    print("even")
+else:
+    print("odd")
