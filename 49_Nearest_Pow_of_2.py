33d9388a64dd72a935158626095616d9fa768ebc
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-11 16:47:27 +0530

    Create 49_Nearest_Pow_of_2.py

---
+++ 49_Nearest_Pow_of_2.py
@@ -0,0 +1,8 @@
+# Hello World program in Python
+#guvi this is rocky
+#rockys aim is getting output without using a single variable
+a=int(input())
+for i in range(0,10):
+    if (2**i)>a:
+        print(2**i)
+        break
