64bd8a31b6fc4eda6357f933fb679b86ee65b64c
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-11 17:01:27 +0530

    Create 53_Roundoff.py

---
+++ 53_Roundoff.py
@@ -0,0 +1,11 @@
+# Hello World program in Python
+#guvi this is rocky
+#rockys aim is getting output without using a single variable
+a=input()
+n=""
+for i in a:
+    if i==".":
+        break
+    else:
+        n+=i
+print(int(n)+1)
