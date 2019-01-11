48f73dd2423847d6ca0206ee1faf9e76a566fc56
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-11 16:16:02 +0530

    Create 43_Add_O_or_Even.py

---
+++ 43_Add_O_or_Even.py
@@ -0,0 +1,9 @@
+# Hello World program in Python
+#guvi this is rocky
+#rockys aim is getting out[ut without using a single variable
+#but this problem surely requires variable OMG
+a,b=map(int,input().split())
+if (a+b)%2==0:
+    print("even")
+else:
+    print("odd")
