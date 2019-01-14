ea40ee7ca030a8814ab71e307c0d8bdc800aeeee
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-14 11:13:38 +0530

    Create 59_EVe_Odd_pos_and_no.py

---
+++ 59_EVe_Odd_pos_and_no.py
@@ -0,0 +1,14 @@
+#hi guvi i am rocky from home towny
+n=int(input())
+l=list(map(int,input().split(" ")))
+ans=[]
+for i in range(len(l)):
+    if i%2==0 and l[i]%2==1:
+        ans.append(l[i])
+    elif i%2==1 and l[i]%2==0:
+        ans.append(l[i])
+for i in range(len(ans)):
+    if i==len(ans)-1:
+        print(ans[i],end="")
+    else:
+        print(ans[i],end=" ")
