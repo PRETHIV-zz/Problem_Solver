2e85f09fe58acb8ee5549634092a3333bec13867
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-14 11:27:14 +0530

    Update 59_EVe_Odd_pos_and_no.py

--- 59_EVe_Odd_pos_and_no.py
+++ 59_EVe_Odd_pos_and_no.py
@@ -1,6 +1,14 @@
+# Hello World program in Python
+    
 #hi guvi i am rocky from home towny
 n=int(input())
-l=list(map(int,input().split(" ")))
+s=input()
+l=[]
+for i in s:
+    try:
+        l.append(int(i))
+    except:
+        continue
 ans=[]
 for i in range(len(l)):
     if i%2==0 and l[i]%2==1:
@@ -9,6 +17,7 @@ for i in range(len(l)):
         ans.append(l[i])
 for i in range(len(ans)):
     if i==len(ans)-1:
-        print(ans[i],end=" ")
+        print(ans[i],end=" ") 
+        #hey manual reviewer here also i am using end=" " coz one case failed showing msg like expected op:1 2 1 ur op:1 2 1 but i used end="" at that time so only i changed
     else:
         print(ans[i],end=" ")
