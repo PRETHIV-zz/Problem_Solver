e840b3f66647557d9d612f067ba64a75fa225335
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-14 22:26:16 +0530

    Create 66_2D_Intersect.py

---
+++ 66_2D_Intersect.py
@@ -0,0 +1,17 @@
+#guvi this is rocky
+n,m=map(int,input().split(" "))
+l=[]
+ans=[]
+for i in range(n):
+    lt=set(map(int,input().split(" ")))
+    l.append(lt)
+a=l[0]
+for i in range(1,n):
+    a=a.intersection(l[i])
+    ans.append(a)
+b=list(ans[len(ans)-1])
+for i in range(len(b)):
+    if i==len(b)-1:
+        print(b[i],end="")
+    else:
+        print(b[i],end=" ")
