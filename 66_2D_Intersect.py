7b7584b4d87cdcea58118b0216e019c3254a30d8
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-15 07:34:53 +0530

    Update 66_2D_Intersect.py

--- 66_2D_Intersect.py
+++ 66_2D_Intersect.py
@@ -1,9 +1,9 @@
 #guvi this is rocky
-n,m=map(int,input().split(" "))
+n,m=map(int,input().split())
 l=[]
 ans=[]
 for i in range(n):
-    lt=set(map(int,input().split(" ")))
+    lt=set(map(int,input().split()))
     l.append(lt)
 a=l[0]
 for i in range(1,n):
