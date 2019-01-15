f08200f9937163fd7f0a0b14e4f07e43e9005884
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-15 08:10:55 +0530

    Create 67_K_In_N.py

---
+++ 67_K_In_N.py
@@ -0,0 +1,13 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+flag=0
+for i in range(len(l)):
+    for j in range(i+1,len(l)):
+        if l[i]+l[j]==k:
+            flag=1
+            print("YES")
+            break
+        if flag==1:
+            break
+if flag==0:
+    print("NO")
