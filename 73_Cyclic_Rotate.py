8522e6800b2ec393ee54ede941bef8f3caadf42a
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-16 12:48:04 +0530

    Update 73_Cyclic_Rotate.py

--- 73_Cyclic_Rotate.py
+++ 73_Cyclic_Rotate.py
@@ -4,4 +4,8 @@ l=list(map(int,input().split()))
 l1=l.copy()
 for i in range(len(l)):
     l1[(i+k)%n]=l[i]
-print(l1)
+for i in range(len(l1)):
+    if i==len(l1)-1:
+        print(l1[i],end="")
+    else:
+        print(l1[i],end=" ")
