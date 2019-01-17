0393b4dee10632730949a06a6a5f0eaecab8dfc9
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-17 18:39:37 +0530

    Create 83_List_Rev.c

---
+++ 83_List_Rev.c
@@ -0,0 +1,19 @@
+//Backing Up 2 Master C again
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+int a[n],i;
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+for(i=n-1;i>=0;i--){
+if(i==0){
+    printf("%d",a[i]);
+}
+else{
+    printf("%d->",a[i]);
+}
+}
+return 0;
+}
