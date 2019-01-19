6796a3a8e492a664f61cf043f64c926e3f3d15da
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-19 18:37:15 +0530

    Update 85_Pnt_Lie_On.c

--- 85_Pnt_Lie_On.c
+++ 85_Pnt_Lie_On.c
@@ -5,10 +5,10 @@ int x1,x2,x3,y1,y2,y3;
 scanf("%d%d",&x1,&y1);
 scanf("%d%d",&x2,&y2);
 scanf("%d%d",&x3,&y3);
-if(x1==x2&&1==x3){
+if(x1==x2&&x1==x3){
 printf("yes");
 }
-else if(y1==y2&&1==y3){
+else if(y1==y2&&y1==y3){
 printf("yes");
 }
 else{
