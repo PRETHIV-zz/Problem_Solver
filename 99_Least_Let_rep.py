7f427ba3177bc2ca6c9aba7ad747ab18d6d83ad7
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-20 15:36:49 +0530

    Create 99_Least_Let_rep.py

---
+++ 99_Least_Let_rep.py
@@ -0,0 +1,19 @@
+#Mr.360
+s=input()
+f=[]
+for i in s:
+    c=-1
+    for j in s:
+        if ord(i)==ord(j) or abs(ord(i)-ord(j))==32:
+            c+=1
+    f.append(c)
+m=min(f)
+ans=[]
+for i in range(len(f)):
+    if m==f[i] and not s[i].isspace():
+        ans.append(s[i])
+for i in range(len(ans)):
+    if i==len(ans)-1:
+        print(ans[i].lower(),end="")
+    else:
+        print(ans[i].lower(),end=" ")
