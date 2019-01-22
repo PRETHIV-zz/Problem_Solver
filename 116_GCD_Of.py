18adc60c4740ebfe3ae5eff4a3e4217460bcf185
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-22 18:19:03 +0530

    Create 116_GCD_Of.py

---
+++ 116_GCD_Of.py
@@ -0,0 +1,31 @@
+#GCD Of 2 nos
+n,q=map(int,input().split())
+lt=list(map(int,input().split()))
+l=[]
+r=[]
+ans=[]
+for i in range(q):
+    a,b=map(int,input().split())
+    l.append(a)
+    r.append(b)
+for i in range(q):
+    a=l[i]
+    b=r[i]
+    if a==b:
+        ans.append(lt[a-1])
+    else:
+        chk=[]
+        for j in range(a-1,b):
+            chk.append(lt[j])
+        gcd=1
+        lim=max(chk)
+        lchk=len(chk)
+        for k in range(1,lim+1):
+            c=0
+            for kk in range(len(chk)):
+                if chk[kk]%k==0:
+                    c+=1
+            if c==lchk and k>=gcd:
+                ans.append(k)
+for i in ans:
+    print(i)
