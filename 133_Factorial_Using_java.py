a99173511ecd721dfea205adfbb28065151a5429
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-24 16:44:05 +0530

    Update 133_Factorial_Using_java.py

--- 133_Factorial_Using_java.py
+++ 133_Factorial_Using_java.py
@@ -1,27 +1,10 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		int n,ans=1;
-		Scanner ss=new Scanner(System.in);
-		n=ss.nextInt();
-		if(n==0){
-			System.out.println(ans);
-		}
-		else{
-			while(n>=1){
-				ans*=n;
-				n--;
-			}
-			System.out.println(ans);
-		}
-	}
-}
+#hlo guvi i am pret
+n=int(input())
+ans=1
+if n==0:
+    print(ans)
+else:
+    while n>=1:
+        ans*=n
+        n-=1
+    print(ans)
