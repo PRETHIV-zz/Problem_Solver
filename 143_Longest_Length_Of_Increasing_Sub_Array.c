5ae79564c81f2e331d82ff47d505e179f83426e3
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-26 10:05:08 +0530

    Create 143_Longest_Length_Of_Increasing_Sub_Array.c

---
+++ 143_Longest_Length_Of_Increasing_Sub_Array.c
@@ -0,0 +1,35 @@
+//Length_Of_Continuos
+//Welcome Stalkers
+int main()
+{
+int n;
+scanf("%d",&n);
+int a[n],i,l[n],j;
+for(i=0;i<n-1;i++){
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++){
+    int c=0;
+    for(j=i;j<n-1;j++){
+        if(a[j]<a[j+1]){
+            c++;
+
+        }
+        else{
+            break;
+        }
+    }
+    l[i]=c+1;
+
+}
+int m=l[0];
+
+for(i=0;i<n;i++){
+    if(m<l[i]){
+        m=l[i];
+    }
+
+}
+printf("%d",m);
+return 0;
+}
