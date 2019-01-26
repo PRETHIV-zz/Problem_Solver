04ba2cec66f9a2bb850d868e1a88c461663a771f
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-26 11:58:09 +0530

    Create 147_And_Of_The_Array.c

---
+++ 147_And_Of_The_Array.c
@@ -0,0 +1,18 @@
+//biwise and
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+int i,a[n];
+for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+
+}
+int ans=a[0];
+for(i=1;i<n;i++){
+    ans=ans&a[i];
+}
+printf("%d",ans);
+return 0;
+}
