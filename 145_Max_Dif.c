07d5d6c777b42d7e33998327e1fe2c69536f133e
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-26 11:20:46 +0530

    Create 145_Max_Dif.c

---
+++ 145_Max_Dif.c
@@ -0,0 +1,26 @@
+//Max_Dif
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+    }
+    int df[n*n],j;
+    static int k=0;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            df[k++]=abs(a[i]-a[j]);
+        }
+    }
+    int m=df[0];
+    for(i=0;i<n*n;i++){
+        if(df[i]>m){
+            m=df[i];
+        }
+    }
+    printf("%d",m);
+return 0;
+}
