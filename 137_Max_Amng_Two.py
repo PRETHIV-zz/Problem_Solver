7a98767082fbca908596a104750bd76e24dd4ae4
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-26 08:30:05 +0530

    Update 137_Max_Amng_Two.py

--- 137_Max_Amng_Two.py
+++ 137_Max_Amng_Two.py
@@ -9,14 +9,9 @@ def pretmax(n1,n2):
         return n2
     else:
         return n1
-lim=n
-if n%2==1:
-    lim=n-1
-for i in range(0,lim,2):
+for i in range(0,n-1,1):
     t=pretmax(a[i],a[i+1])
     ans.append(t)
-if n%2==1:
-    ans.append(a[n-1])
 for i in range(len(ans)):
     if i==len(ans)-1:
         print(ans[i],end="")
