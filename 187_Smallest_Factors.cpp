7f97aecf145b06075ff47d785857ce488acd1937
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-27 14:55:06 +0530

    Create 187_Smallest_Factors.cpp

---
+++ 187_Smallest_Factors.cpp
@@ -0,0 +1,30 @@
+//smallst facctor
+#include<iostream>
+using namespace std;
+bool chkfact(int no,int f){
+    if(no%f==0){return true;}
+    else{return false;}
+}
+int main(){
+int n;
+cin>>n;
+int a[n];
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+int i=2;
+while(true){
+    int c=0;
+    for(int j=0;j<n;j++){
+        if(chkfact(i,a[j])){
+            c++;
+        }
+    }
+    if(c==n){
+        break;
+        }
+        i++;
+}
+cout<<i;
+return 0;
+}
