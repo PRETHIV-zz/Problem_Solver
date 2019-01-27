dfd8f82cc111817a840d9794d4f418474101df6e
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-27 14:27:19 +0530

    Create 185_Month_Print.py

---
+++ 185_Month_Print.py
@@ -0,0 +1,5 @@
+#Month_In_Words
+n=list(map(int,input().split("-")))
+mnth=n[1]
+s=["","January","February","March","April","May","June","July","August","September","October","November","December"]
+print(s[mnth])
