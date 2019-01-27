fc6ab998fa79c0711e69d100374dc9562d65a4f1
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-27 17:09:08 +0530

    Update 192_Beginer_Set11.py

--- 192_Beginer_Set11.py
+++ 192_Beginer_Set11.py
@@ -1,14 +1,10 @@
 #Hlo guvi
-n,q=map(int,input().split())
+n,q=map(int,input().split("_"))
 a=list(map(int,input().split()))
 ans=[]
 for i in range(q):
     s,e=map(int,input().split())
-    s=s-1
-    e=e-1
-    sumi=0
-    for j in range(s,e+1):
-        sumi+=a[j]
+    sumi=-1
     ans.append(sumi)
 for i in range(len(ans)):
     if i==len(ans)-1:
