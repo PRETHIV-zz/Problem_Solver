d92fc5b7bb950c630689994ee9fedf370565ea20
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-29 22:00:05 +0530

    Update 236_XOR_CHK.py

--- 236_XOR_CHK.py
+++ 236_XOR_CHK.py
@@ -2,11 +2,5 @@
 n,q=map(int,input().split())
 a=list(map(int,input().split()))
 ans=[]
-for i in range(q):
-    u,v=map(int,input().split())
-    val=a[u-1]
-    for j in range(u,v):
-        val^=a[j]
-    ans.append(val)
 for i in ans:
     print(i)
