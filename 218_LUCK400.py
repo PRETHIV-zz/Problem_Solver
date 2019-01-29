007b1cfd0ca837be4068a0524c2ee3f936c8af12
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-29 15:41:50 +0530

    Create 218_LUCK400.py

---
+++ 218_LUCK400.py
@@ -0,0 +1,23 @@
+#Ready to Hunt by rocky
+def ispalin(s):
+    ss=s[::-1]
+    if s==ss:
+        return True
+    else:
+        return False
+s=input()
+ans=[]
+c=0
+for i in range(len(s)):
+    a=""
+    for j in range(len(s)):
+        if i!=j:
+            a+=s[j]
+    ans.append(a)
+for i in ans:
+    if ispalin(i):
+        print("YES")
+        c+=1
+        break
+if c==0:
+    print("NO")
