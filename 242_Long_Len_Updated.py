62be31e5a10461e5e8cee2f571e1173db2164ae1
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-29 23:00:59 +0530

    Create 242_Long_Len_Updated.py

---
+++ 242_Long_Len_Updated.py
@@ -0,0 +1,19 @@
+#Increasing sub prethiv
+def coutincr(a,i):
+    l=i+1
+    m=len(a)
+    if l==m:
+        return 1
+    else:
+        if a[i]<=a[l]:
+            return 1+coutincr(a,i+1)
+        else:
+            return 1
+n=int(input())
+a=list(map(int,input().split()))
+m=0
+for i in range(len(a)):
+    val=coutincr(a,i)
+    if val>m:
+        m=val
+print(m)
