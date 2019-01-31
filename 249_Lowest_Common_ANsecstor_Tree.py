aac52bfa6773588b9a536387a59af6f9f6fde68c
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-31 16:13:52 +0530

    Update 249_Lowest_Common_ANsecstor_Tree.py

--- 249_Lowest_Common_ANsecstor_Tree.py
+++ 249_Lowest_Common_ANsecstor_Tree.py
@@ -16,16 +16,22 @@ def insert(root,ins):
     elif ins.data<=root.data and root.left==None:
         root.left=ins
         return
-
+def inorder(root):
+    if root is None:
+        return
+    else:
+        inorder(root.left)
+        print(root.data,end=" ")
+        inorder(root.right)
 def lcafinder(root,n1,n2):
     if root is None:
         return None
     if root.data>n1 and root.data>n2:
-        lcafinder(root.left,n1,n2)
+        return lcafinder(root.left,n1,n2)
     if root.data<n1 and root.data<n2:
-        lcafinder(root.right,n1,n2)
+        return lcafinder(root.right,n1,n2)
     return root.data
-    
+
 n=int(input())
 a=list(map(int,input().split()))
 l,r1=map(int,input().split())
@@ -33,4 +39,5 @@ r=Node(a[0])
 for i in  range(1,n):
     nn=Node(a[i])
     insert(r,nn)
+#inorder(r)
 print(lcafinder(r,l,r1))
