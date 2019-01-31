5ce512878d24c2d6b098cf935d11dbdf56492f77
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-01-31 16:03:19 +0530

    Create 249_Lowest_Common_ANsecstor_Tree.py

---
+++ 249_Lowest_Common_ANsecstor_Tree.py
@@ -0,0 +1,36 @@
+#Lowest Common Ancestor
+class Node:
+    def __init__(self,d):
+        self.data=d
+        self.left=None
+        self.right=None
+
+def insert(root,ins):
+    if ins.data>root.data and root.right!=None:
+        insert(root.right,ins)
+    elif ins.data<=root.data and root.left!=None:
+        insert(root.left,ins)
+    if ins.data>root.data and root.right==None:
+        root.right=ins
+        return
+    elif ins.data<=root.data and root.left==None:
+        root.left=ins
+        return
+
+def lcafinder(root,n1,n2):
+    if root is None:
+        return None
+    if root.data>n1 and root.data>n2:
+        lcafinder(root.left,n1,n2)
+    if root.data<n1 and root.data<n2:
+        lcafinder(root.right,n1,n2)
+    return root.data
+    
+n=int(input())
+a=list(map(int,input().split()))
+l,r1=map(int,input().split())
+r=Node(a[0])
+for i in  range(1,n):
+    nn=Node(a[i])
+    insert(r,nn)
+print(lcafinder(r,l,r1))
