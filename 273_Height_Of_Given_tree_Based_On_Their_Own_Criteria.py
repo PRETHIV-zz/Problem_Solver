4f68b4b669b5aa9a85d954e01f7a7dd2b4029491
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-02-02 17:03:52 +0530

    Create 273_Height_Of_Given_tree_Based_On_Their_Own_Criteria.py

---
+++ 273_Height_Of_Given_tree_Based_On_Their_Own_Criteria.py
@@ -0,0 +1,70 @@
+#it is not like an ordinary binary tree where the lesser andlarger elements are arranged in a manner
+#Rather they will give the parent and child node we need to construct a tree first based on the given condition
+#and then we have to find the height of the tree
+#i used level order traversal for parent node searching in each step since it is going to be the easiest method
+# and i used queee method to find unlike ordinary printing technique
+#DS Problem_Solve _Pandradhunale oru thani gethu dhana sir
+class Tree:
+    def __init__(self,d):
+        self.data=d
+        self.left=None
+        self.right=None
+
+def findroot(root,n):
+    q=[]
+    q.append(root)
+    while len(q)>0:
+        t=q.pop(0)
+        if t.data==n:
+            return t
+        if t.left!=None:
+            q.append(t.left)
+        if t.right!=None:
+            q.append(t.right)
+def insertele(root,ins):
+    if root.left==None:
+        root.left=ins
+        return
+    if root.right==None:
+        root.right=ins
+        return
+    if ins.data>root.data:
+        insertele(root.right,ins)
+        return
+    else:
+        insertele(root.left,ins)
+        return
+def lvlorder(root):
+    q=[]
+    q.append(root)
+    while len(q)>0:
+        t=q.pop(0)
+        print(t.data,end=" ")
+        if t.left!=None:
+            q.append(t.left)
+        if t.right!=None:
+            q.append(t.right)
+def findheight(root):
+    if root is None:
+        return 0
+    else:
+        l=findheight(root.left)
+        r=findheight(root.right)
+        if l>r:
+            return l+1
+        else:
+            return r+1
+n,k=map(int,input().split())
+root=Tree(None)
+for i in range(k):
+    if i==0:
+        r,nn=map(int,input().split())
+        root=Tree(r)
+        newnode=Tree(nn)
+        insertele(root,newnode)
+    else:
+        p,c=map(int,input().split())
+        temp=findroot(root,p)
+        newnode=Tree(c)
+        insertele(temp,newnode)
+print(findheight(root))
