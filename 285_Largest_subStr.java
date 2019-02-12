d03bc5dbd8e75ef48128ce499b1e2a5926b8c9d2
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-02-12 19:20:26 +0530

    Create 285_Largest_subStr.java

---
+++ 285_Largest_subStr.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s,ans="",t1="";
+		Scanner ss=new Scanner(System.in);
+		s=ss.next();
+		t1=s;
+		for(int i=0;i<s.length();i++){
+			String temp="";
+			for(int j=i+1;j<s.length();j++){
+				temp=temp+s.charAt(j);
+			}
+			if(temp.compareTo(t1)>0){
+				ans=temp;
+				t1=ans;
+			}
+		}
+		System.out.println(ans);
+	}
+}
