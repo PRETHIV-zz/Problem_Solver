9654e2717f5c44bd385798ecef3be6cad98bf44b
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-03-09 19:23:24 +0530

    Update 298_Prime_Series.py

--- 298_Prime_Series.py
+++ 298_Prime_Series.py
@@ -16,4 +16,8 @@ for i in range(n):
         l.append(i)
     else:
         continue
-print(*l)
+for i in range(len(l)):
+    if i==len(l)-1:
+        print(l[i],end="")
+    else:
+        print(l[i],end=" ")
