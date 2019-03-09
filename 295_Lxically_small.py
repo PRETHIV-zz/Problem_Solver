800c8024166847e6e66776edc36f7200396e22d3
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-03-09 17:58:17 +0530

    Create 295_Lxically_small.py

---
+++ 295_Lxically_small.py
@@ -0,0 +1,24 @@
+#Lexically_Basically
+s=input()
+l=list(s)
+l.sort()
+l.reverse()
+rev=""
+for i in l:
+    rev+=i
+if rev==s:
+    print(-1)
+else:
+    o=list(s)
+    #print(o)
+    for i in range(len(o)-1,0,-1):
+        #print(o[i])
+        if ord(o[i])>ord(o[i-1]):
+            t=o[i]
+            o[i]=o[i-1]
+            o[i-1]=t
+            break
+    ans=""
+    for i in o:
+        ans+=i
+    print(ans)
