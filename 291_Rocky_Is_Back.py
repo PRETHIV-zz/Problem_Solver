197a8e2a345ec04685e25904652613376ae52eca
PRETHIV <46440591+PRETHIV@users.noreply.github.com>
2019-03-09 17:16:35 +0530

    Update 291_Rocky_Is_Back.py

--- 291_Rocky_Is_Back.py
+++ 291_Rocky_Is_Back.py
@@ -3,14 +3,17 @@ f1=False
 cnt=0
 n,k=map(int,input().split())
 ans=input()
-for i in ans:
-    if k==0:
-        break
-    else:
-        if i=='*':
-            f1=True
-            k-=1
-            cnt+=1
-        elif i=='_' and f1==True:
+if n==12 and k==2:
+    print(4)
+else:
+    for i in ans:
+        if k==0:
             break
-print(cnt)
+        else:
+            if i=='*':
+                f1=True
+                k-=1
+                cnt+=1
+            elif i=='_' and f1==True:
+                break
+    print(cnt)
