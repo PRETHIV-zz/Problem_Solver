41d70d92b2d73f09110608db0e124acba0a95397
prethivashok <46440591+PRETHIV@users.noreply.github.com>
2020-01-29 14:20:18 +0530

    Add files via upload

--- Trial UI 1 - Copy.py
+++ Trial UI 1 - Copy.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import tkinter.messagebox as Mbox
 from pygame import mixer
+from imageai.Detection import ObjectDetection
 #********************************************************#
 #Recognize function
 def Recognize():
@@ -174,6 +175,155 @@ def Search_Pc(words):
 def Shut_PC():
     os.system("shutdown /s /t 1")
 #********************************************************#
+def Seperate(e1):
+    exec_path=e1.get()
+    cur_path=os.getcwd()
+    os.chdir(exec_path)
+    time.sleep(5)
+    try:
+        os.makedirs("images")
+        os.makedirs("study_materials")
+        os.makedirs("audio")
+        os.makedirs("video")
+    except:
+        pass
+    for r,d,f in os.walk(os.getcwd()):
+        for file in f:
+            if '.jpg' in file or '.jpeg' in file:
+                o="move "+"\""+exec_path+"\\"+file+"\" "+"\""+exec_path+"\\images\""
+                os.system(o)
+                print(o)
+            elif '.mp3' in file:
+                o="move "+"\""+exec_path+"\\"+file+"\" "+"\""+exec_path+"\\audio\""
+                os.system(o)
+                print(o)
+            elif '.mp4' in file:
+                o="move "+"\""+exec_path+"\\"+file+"\" "+"\""+exec_path+"\\video\""
+                os.system(o)
+                print(o)
+            elif '.docx' in file or '.pdf' in file:
+                o="move "+"\""+exec_path+"\\"+file+"\" "+"\""+exec_path+"\\study_materials\""
+                os.system(o)
+                print(o)
+        break
+    Mbox.showinfo("Your Files have been Seperated","Task Done")
+    os.chdir(cur_path)
+    
+#********************************************************#
+def Seperatefiles():
+    h1=Tk()
+    h1.geometry("300x150")
+    h1.title("Seperate Files")
+
+    l1a=Label(h1,text='Execution Path')
+    l1a.config(font=("Century Gothic", 10))
+    l1a.grid(row=0,column=0)
+    
+    e2=Entry(h1)
+    e2.grid(row=0,column=1)
+    
+    b1a=Button(h1,text='Seperate Files',command=lambda:Seperate(e2))
+    b1a.grid(row=2,column=0)
+
+    h1.mainloop()
+
+#********************************************************#
+def ObjectPicker(e2,e3):
+    item=e2.get()
+    cur_path=os.getcwd()
+    execution_path = e3.get()
+    os.chdir(execution_path)
+    try:
+        os.makedirs("selectedimages")
+    except:
+        pass
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath( os.path.join(cur_path, "resnet50_coco_best_v2.0.1.h5"))
+    detector.loadModel()
+    #iname=input("Enter the image i want to analyse :")
+    #firstname=""
+    #for i in iname:
+    #    if i=='.':
+    #        break
+    #    else:
+    #        firstname+=i
+    #os.chdir("selectedimages")
+    for r,d,f in os.walk(execution_path):
+        for file in f:
+            if '.jpg' in file:
+                firstname=list(file.split('.'))
+                firstname=firstname[0]
+                detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path ,file), output_image_path=os.path.join(execution_path , firstname+"_Analysed.jpg"))
+                for eachObject in detections:
+                    if eachObject["name"]==item:
+                        o="copy "+"\""+execution_path+"\\"+file+"\" "+"\""+execution_path+"\\selectedimages\""
+                        print(o)
+                        os.system(o)
+        break
+        #print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+    #print("End _of _our _sneak Peak Coded by rocky")
+    Mbox.showinfo("Info","Task Finished")
+    os.chdir(cur_path)
+#********************************************************#
+def PickObjects():
+    h1=Tk()
+    h1.geometry("300x150")
+    h1.title("Pick Objects")
+
+    l1a=Label(h1,text='Object Name ')
+    l1a.config(font=("Century Gothic", 10))
+    l1a.grid(row=0,column=0)
+    
+    e2=Entry(h1)
+    e2.grid(row=0,column=1)
+
+    l1a=Label(h1,text='Execution Path ')
+    l1a.config(font=("Century Gothic", 10))
+    l1a.grid(row=1,column=0)
+
+    e3=Entry(h1)
+    e3.grid(row=1,column=1)
+    
+    b1a=Button(h1,text='Pickup Photos',command=lambda:ObjectPicker(e2,e3))
+    b1a.grid(row=2,column=0)
+
+    h1.mainloop()
+#********************************************************#
+def GoogleIt(words):
+    search=""
+    cmd="https://www.google.com/search?q="
+    for_found=False
+    for i in words:
+        if for_found:
+            search+=i
+            search+='%20'
+        elif i=='for' or i=='For':
+            for_found=True
+    os.system("start "+cmd+search)
+#********************************************************#
+def GoogleIt1(s):
+    temp=''
+    for i in s:
+        if i.isspace():
+            temp+='%20'
+        else:
+            temp+=i
+    cmd="https://www.google.com/search?q="
+    os.system("start "+cmd+temp)
+#********************************************************#
+def Youtube(words):
+    temp=""
+    play=False
+    for i in words:
+        if play:
+            temp+=i
+            temp+='%20'
+        elif i=='play' or i=='Play':
+            play=True
+    cmd="https://www.youtube.com/results?search_query="
+    os.system("start "+cmd+temp)
+#********************************************************#
 def Ordered(e1):
     s=e1.get()
     words=list(s.split())
@@ -186,6 +336,16 @@ def Ordered(e1):
         Search_Pc(words)
     elif 'shutdown' in words or 'Shutdown' in words or 'SHUTDOWN' in words:
         Shut_PC()
+    elif 'seperatefiles' in words or 'seperate' in words or 'separate' in words or 'Seperatefiles' in words or 'SEPERATEFILES' in words or 'Seperate' in words or 'SEPERATE' in words:
+        Seperatefiles()
+    elif 'pickup' in words or 'Pickup' in words or 'PICKUP' in words or 'object' in words or 'Object' in words or 'OBJECT' in words: 
+        PickObjects()
+    elif 'google' in words or 'Google' in words:
+        GoogleIt(words)
+    elif 'play' in words or 'Play' in words:
+        Youtube(words)
+    else:
+        GoogleIt1(s)
 #********************************************************#
 
 root=Tk()
